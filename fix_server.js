af36158f723605e9daac60225d11f08dd7baaf87
Omkarratnani <ratnani.omkar123@gmail.com>
2026-06-17 10:36:06 +0530

    chore: clean up unnecessary and backup files

--- fix_server.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let src = fs.readFileSync('server.mjs', 'utf8');
-src = src.replace(/\\\`/g, '\`');
-src = src.replace(/\\\$/g, '$');
-fs.writeFileSync('server.mjs', src);
-console.log('Fixed backticks in server.mjs');
